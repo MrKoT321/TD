8061591e4380582c8d49dd57fa21a6016940977c
SVLOGer <max443584@gmail.com>
2023-07-04 15:16:34 +0300

    mmm

--- static/js/monster_movement.js
+++ static/js/monster_movement.js
@@ -1,10 +1,21 @@
-var monster = {
+var sf = {
     hp: monster1.hp,
     speed: monster1.speed,
     cost: monster1.cost,
     width: monster1.width,
     height: monster1.height,
     color: monster1.color,
+    x: lvl1.start_x,
+    y: lvl1.start_y
+}
+
+var pa = {
+    hp: monster2.hp,
+    speed: monster2.speed,
+    cost: monster2.cost,
+    width: monster2.width,
+    height: monster2.height,
+    color: monster2.color,
     x: lvl1.start_x,
     y: lvl1.start_y
 }
--- static/js/monsters.js
+++ static/js/monsters.js
@@ -1,6 +1,6 @@
 var monster1 = {
     hp: 100,
-    speed: 3,
+    speed: 2,
     cost: 0,
     width: 50,
     height: 50,
@@ -9,7 +9,7 @@ var monster1 = {
 
 var monster2 = {
     hp: 100,
-    speed: 3,
+    speed: 2,
     cost: 0,
     width: 50,
     height: 50,
