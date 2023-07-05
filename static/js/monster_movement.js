86afb15e5f5d26dfacb9dcea29a58dbe33eb170a
SVLOGer <max443584@gmail.com>
2023-07-05 10:37:22 +0300

    add moveMonsters

--- static/js/monster_movement.js
+++ static/js/monster_movement.js
@@ -1,28 +1,15 @@
 var monsters = [];
+var notdeadmonsters = [];
 
-var sf = {
-    hp: monster1.hp,
-    speed: monster1.speed,
-    cost: monster1.cost,
-    width: monster1.width,
-    height: monster1.height,
-    color: monster1.color,
-    x: lvl1.start_x,
-    y: lvl1.start_y,
-    dir: lvl1.start_dir
-}
+var sf = monster1;
+sf.x = lvl1.start_x - 50;
+sf.y = lvl1.start_y;
+sf.dir = lvl1.start_dir;
 
-var pa = {
-    hp: monster2.hp,
-    speed: monster2.speed,
-    cost: monster2.cost,
-    width: monster2.width,
-    height: monster2.height,
-    color: monster2.color,
-    x: lvl1.start_x,
-    y: lvl1.start_y,
-    dir: lvl1.start_dir
-}
+var pa = monster2;
+pa.x = lvl1.start_x - 50;
+pa.y = lvl1.start_y;
+pa.dir = lvl1.start_dir;
 
 // function updateMonster(monster){
 //     monster.x += monster.speed;
@@ -32,13 +19,12 @@ function drawMonster(monster) {
     canvasContext.fillStyle = "rgba(0, 0, 0, 0)";
     canvasContext.fillRect(0, 0, 1000, 1000);
     canvasContext.fillStyle = monster.color;
-    canvasContext.fillRect(monster.x, monster.y - monster.height/2, monster.width, monster.height);
-    // canvasContext.fill();
+    canvasContext.fillRect(monster.x, monster.y - monster.height / 2, monster.width, monster.height);
 }
 
 function monsterMove(monster) {
-    switch(monster.dir) {
-        case 'r': 
+    switch (monster.dir) {
+        case 'r':
             monster.x += monster.speed;
             break;
         case 'u':
@@ -68,11 +54,11 @@ function contains(arr, elem) {
 }
 
 function monsterCorrect(lvl, monster) {
-    switch(monster.dir) {
+    switch (monster.dir) {
         case 'r':
-            if(!contains(lvl.road, canvasToGrid(monster.x + monster.width/2 + 100, monster.y))) {
-                if(contains(lvl.road, canvasToGrid(monster.x, monster.y - 100))) {
-                    console.log('клетка:', canvasToGrid(monster.x + monster.width/2 + 100, monster.y));
+            if (!contains(lvl.road, canvasToGrid(monster.x + monster.width / 2 + 100, monster.y))) {
+                if (contains(lvl.road, canvasToGrid(monster.x, monster.y - 100))) {
+                    console.log('клетка:', canvasToGrid(monster.x + monster.width / 2 + 100, monster.y));
                     monster.dir = 'u';
                 } else {
                     monster.dir = 'd';
@@ -80,8 +66,8 @@ function monsterCorrect(lvl, monster) {
             }
             break;
         case 'u':
-            if(!contains(lvl.road, canvasToGrid(monster.x, monster.y - 100))) {
-                if(contains(lvl.road, canvasToGrid(monster.x - 110, monster.y))) {
+            if (!contains(lvl.road, canvasToGrid(monster.x, monster.y - 100))) {
+                if (contains(lvl.road, canvasToGrid(monster.x - 110, monster.y))) {
                     monster.dir = 'l';
                 } else {
                     monster.dir = 'r';
@@ -89,8 +75,8 @@ function monsterCorrect(lvl, monster) {
             }
             break;
         case 'l':
-            if(!contains(lvl.road,canvasToGrid(monster.x - monster.width/2 - 100, monster.y))) {
-                if(contains(lvl.road, canvasToGrid(monster.x + 110, monster.y + 110))) {
+            if (!contains(lvl.road, canvasToGrid(monster.x - monster.width / 2 - 100, monster.y))) {
+                if (contains(lvl.road, canvasToGrid(monster.x + 110, monster.y + 110))) {
                     monster.dir = 'u';
                 } else {
                     monster.dir = 'd';
@@ -98,8 +84,8 @@ function monsterCorrect(lvl, monster) {
             }
             break;
         case 'd':
-            if(!contains(lvl.road, canvasToGrid(monster.x, monster.y + 100))) {
-                if(contains(lvl.road, canvasToGrid(monster.x - 110, monster.y))) {
+            if (!contains(lvl.road, canvasToGrid(monster.x, monster.y + 100))) {
+                if (contains(lvl.road, canvasToGrid(monster.x - 110, monster.y))) {
                     monster.dir = 'l';
                 } else {
                     monster.dir = 'r';
@@ -107,21 +93,41 @@ function monsterCorrect(lvl, monster) {
             }
             break;
     }
-    
+
 }
 
-function addMonster(monster, lvl){
-    monsters.push ({
+function addMonster(monster) {
+    monsters.push({
         hp: monster.hp,
         speed: monster.speed,
         cost: monster.cost,
         width: monster.width,
         height: monster.height,
         color: monster.color,
-        x: lvl.start_x - 50,
-        y: lvl.start_y,
-        dir: lvl1.start_dir
+        x: monster.x,
+        y: monster.y,
+        dir: monster.dir
     })
 }
 
-addMonster(sf, lvl1)
+addMonster(sf)
+
+function moveMonsters() {
+    let notdeadmonsters = monsters.filter(value => value.hp > 0);
+    monsters = notdeadmonsters;
+    for (var monster of monsters) {
+        drawMonster(monster);
+    }
+    for (var monster of monsters) {
+        monsterMove(monster);
+        monsterCorrect(lvl, monster);
+    }
+    if (mobamount > 0) {
+        starttime += 2
+        if (starttime > 100) {
+            addMonster(pa);
+            starttime = 0;
+            mobamount -= 1;
+        }
+    }
+}
