45c907ec4e74d087432da040065732c0a0936aac
MrKoT <vladimir.fedoseev.2004@mail.ru>
2023-07-07 20:54:15 +0300

    Попытка запустить сервер

--- src/Controller/ServerController.php
+++ src/Controller/ServerController.php
@@ -5,7 +5,12 @@ class ServerController
     private const HTTP_STATUS_303_SEE_OTHER = 303;
     public function index(): void
     {
-        require __DIR__ . '/../public/TD.html';
+        require __DIR__ . '/menu.html';
+    }
+
+    public function singleGame(): void 
+    {
+        require __DIR__ . '/TD.php';
     }
 
     private function writeRedirectSeeOther(string $url): void
