cdd806be83597ff03c05b1464e49dd0937dd19ce
MrKoT <vladimir.fedoseev.2004@mail.ru>
2023-07-07 21:56:16 +0300

    Теперь у нас есть сервер

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$controller = new App\Controller\ServerController;
+$controller = new App\Controller\ServerController();
 $controller->index();
--- src/Controller/ServerController.php
+++ src/Controller/ServerController.php
@@ -1,16 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App\Controller;
 class ServerController
 {
     private const HTTP_STATUS_303_SEE_OTHER = 303;
     public function index(): void
     {
-        require __DIR__ . '/menu.html';
+        require __DIR__ . '/../../public/pages/menu.html';
     }
 
     public function singleGame(): void 
     {
-        require __DIR__ . '/TD.php';
+        $this->writeRedirectSeeOther("/../../pages/TD.html");
     }
 
     private function writeRedirectSeeOther(string $url): void
