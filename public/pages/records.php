aa9008d9ffd1dec3b4a16a26db2662605de8e7bb
MrKoT <vladimir.fedoseev.2004@mail.ru>
2023-07-12 15:36:51 +0300

    Обнновил таблицу рекордов

--- public/pages/records.php
+++ public/pages/records.php
@@ -13,15 +13,18 @@
     </head>
     <body>
         <div class="buttons">
-            <a href="../" class="back-to-menu">Назад</a>
+            <a href="../" class="back-to-menu">
+                <img src="../static/images/back_arrow.png" class="back-to-menu__img" />
+            </a>
         </div>
         <div class="records">
-            <span class="records-title">Top scores</span>
+            <div class="records-title">Top scores</div>
             <? foreach($records as $score):?>
-                <span><?= htmlspecialchars($score->getNickName()) ?></span>
-                <span><?= $score->getChoisenClass() ?></span>
-                <span><?= $score->getScore() ?></span>
-                <span></span>
+                <div class="record">
+                    <div class="record-nickname"><?= htmlspecialchars($score->getNickName()) ?></div>
+                    <div class="record-choisenclass"><?= $score->getChoisenClass() ?></div>
+                    <div class="record-score"><?= $score->getScore() ?></div>
+                </div>
             <? endforeach; ?>
         </div>
     </body>
