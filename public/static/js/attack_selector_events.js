b74047acf19cd3e4ac556a1b06f9161b6313a1b9
SVLOGer <max443584@gmail.com>
2023-07-13 17:35:18 +0300

    add add and sell functions

---
+++ public/static/js/attack_selector_events.js
@@ -0,0 +1,159 @@
+mob1_selector.addEventListener(
+    "click",
+    () => {
+        curr_mob = monster1;
+        addMobsToWaves();
+    }
+)
+
+mob2_selector.addEventListener(
+    "click",
+    () => {
+        curr_mob = monster2;
+        addMobsToWaves();
+    }
+)
+
+mob3_selector.addEventListener(
+    "click",
+    () => {
+        curr_mob = monster3;
+        addMobsToWaves();
+    }
+)
+
+mob4_selector.addEventListener(
+    "click",
+    () => {
+        curr_mob = monster4;
+        addMobsToWaves();
+    }
+)
+
+mob5_selector.addEventListener(
+    "click",
+    () => {
+        curr_mob = monster5;
+        addMobsToWaves();
+    }
+)
+
+sell1_1.addEventListener(
+    "click",
+    () => {
+        wave1[0].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell2_1.addEventListener(
+    "click",
+    () => {
+        wave1[1].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell3_1.addEventListener(
+    "click",
+    () => {
+        wave1[2].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell4_1.addEventListener(
+    "click",
+    () => {
+        wave1[3].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell5_1.addEventListener(
+    "click",
+    () => {
+        wave1[4].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell1_2.addEventListener(
+    "click",
+    () => {
+        wave2[0].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell2_2.addEventListener(
+    "click",
+    () => {
+        wave2[1].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell3_2.addEventListener(
+    "click",
+    () => {
+        wave2[2].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell4_2.addEventListener(
+    "click",
+    () => {
+        wave2[3].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell5_2.addEventListener(
+    "click",
+    () => {
+        wave2[4].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell1_3.addEventListener(
+    "click",
+    () => {
+        wave3[0].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell2_3.addEventListener(
+    "click",
+    () => {
+        wave3[1].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell3_3.addEventListener(
+    "click",
+    () => {
+        wave3[2].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell4_3.addEventListener(
+    "click",
+    () => {
+        wave3[3].amount -= 1;
+        count_sell = 0;
+    }
+)
+
+sell5_3.addEventListener(
+    "click",
+    () => {
+        wave3[4].amount -= 1;
+        count_sell = 0;
+    }
+)
