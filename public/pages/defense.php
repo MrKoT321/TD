1d5fb5b4d4b886dc89340f134928511c1294dbdd
AlinaDmitrieva12 <alina.dmitrievaa12@gmail.com>
2023-07-18 18:56:41 +0300

    Подключила php и меню

--- public/pages/defense.php
+++ public/pages/defense.php
@@ -67,17 +67,17 @@
                 <div class="buf-slot"></div>
                 <div class="buf-slot"></div>
                 <div class="bar-game-info">
-                    <div class="game-info-lvl game-info-slot">
-                        <span>LVL:</span>
-                        <span>1</span>
+                <div class="game-info-lvl game-info-slot">
+                        <span class="game-info-lvl__title">LVL:</span>
+                        <span id="current-lvl">1</span>
                         <span>/</span>
-                        <span>3</span>
+                        <span id="total-lvl"></span>
                     </div>
                     <div class="game-info-wave game-info-slot">
-                        <span>WAVE:</span>
-                        <span>1</span>
+                        <span class="">WAVE:</span>
+                        <span id="current-wave">1</span>
                         <span>/</span>
-                        <span>3</span>
+                        <span id="total-wave"></span>
                     </div>
                 </div>
             </div>
